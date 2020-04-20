5b3e5f19fc15566a301ba705c3c7de9ae122e214
Artur Okonski <okonski.a.dev@gmail.com>
2020-04-20 09:32:53 +0200

    Clean up controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,18 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :edit, :update, :destroy]
-
-  # GET /games
-  # GET /games.json
   def index
     @games = Games::Game.all
   end
 
-  # GET /games/1
-  # GET /games/1.json
   def show
+    @game = Games::Game.find(params[:id])
   end
 
-  # GET /games/new
   def new
     @game = Games::Game.new
   end
 
-  # GET /games/1/edit
   def edit
+    @game = Games::Game.find(params[:id])
   end
 
   def create
@@ -31,23 +25,12 @@ class GamesController < ApplicationController
     redirect_to games_path, notice: 'Game was successfully added to your collection.'
   end
 
-  # PATCH/PUT /games/1
-  # PATCH/PUT /games/1.json
   def update
-    respond_to do |format|
-      if @game.update(game_params)
-        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
-        format.json { render :show, status: :ok, location: @game }
-      else
-        format.html { render :edit }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
     end
   end
 
-  # DELETE /games/1
-  # DELETE /games/1.json
   def destroy
+    @game = Games::Game.find(params[:id])
     @game.destroy
     respond_to do |format|
       format.html { redirect_to games_url, notice: 'Game was successfully destroyed.' }
@@ -56,13 +39,8 @@ class GamesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Games::Game.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def game_params
-      params.require(:game).permit(:name)
-    end
+  def game_params
+    params.require(:game).permit(:name)
+  end
 end
