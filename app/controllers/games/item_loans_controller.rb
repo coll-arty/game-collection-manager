e06a762e4edb5cb2deba57a4e993d0dc53c3981f
Artur Okonski <okonski.a.dev@gmail.com>
2020-04-23 09:04:39 +0200

    Add Loans controller

---
+++ app/controllers/games/item_loans_controller.rb
@@ -0,0 +1,6 @@
+module Games
+  class ItemLoansController < ApplicationController
+    def new
+    end
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope module: 'games' do
     root 'collection_items#index'
     resources :collection_items
+    resources :item_loans, only: [:new, :create, :destroy]
   end
   mount RailsEventStore::Browser => '/res' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
