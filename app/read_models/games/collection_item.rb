4c12588288c9c75b148281725ade6dcb4f137e75
Artur Okonski <okonski.a.dev@gmail.com>
2020-04-29 08:14:32 +0200

    Associate Loan with GameItem

--- app/read_models/games/collection_item.rb
+++ app/read_models/games/collection_item.rb
@@ -1,5 +1,9 @@
 module Games
   class CollectionItem < ApplicationRecord
     self.table_name = "games_collection_items"
+
+    has_one :loan,
+            class_name: 'Games::Loan',
+            foreign_key: 'games_collection_item_id'
   end
 end
--- app/read_models/games/loan.rb
+++ app/read_models/games/loan.rb
@@ -1,5 +1,9 @@
 module Games
   class Loan < ApplicationRecord
     self.table_name = "games_loans"
+
+    belongs_to :collection_item,
+               class_name: 'Games::CollectionItem',
+               foreign_key: 'games_collection_item_id'
   end
 end
