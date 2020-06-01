aa5094d6b29ffbd37a6f3f20a9e1dacd2621b94c
Artur Okonski <okonski.a.dev@gmail.com>
2020-06-01 08:55:58 +0200

    Add email info to loans

---
+++ db/migrate/20200601065531_add_email_to_games_loans.rb
@@ -0,0 +1,5 @@
+class AddEmailToGamesLoans < ActiveRecord::Migration[6.0]
+  def change
+    add_column :games_loans, :loanee_email, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_064720) do
+ActiveRecord::Schema.define(version: 2020_06_01_065531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_04_27_064720) do
     t.text "loanee", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "loanee_email"
     t.index ["games_collection_item_id"], name: "index_games_loans_on_games_collection_item_id"
   end
 
